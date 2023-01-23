5fc824a606528e6718caaf0d6ad8bc2fc971cfee
Isabelle Oktay <iso212@nyu.edu>
2023-01-23 14:31:42 -0500

    fixed feedback

--- sinewave.cpp
+++ sinewave.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 const int sampleRate = 48000;
 const int bitDepth = 24;
-const int minFreq = 20;
-const int maxFreq = 20000;
+const int minFreq = 0;
+const int maxFreq = 24000;
+const int minDuration = 0;
 const int maxDuration = 30;
-const string fileName = "audio.wav";
+const char *fileName = "audio.wav";
 
 class SineOscillator
 {
@@ -41,52 +42,64 @@ int makeSineWave(float freq, int_least32_t duration)
     {
         throw invalid_argument("Frequency must be in the natural range (0 Hz - 24 kHz).");
     }
-    if (duration < 0 || duration > maxDuration)
+    if (duration < minDuration || duration > maxDuration)
     {
         throw invalid_argument("Duration must be in the range of 0 - 30 seconds.");
     }
 
     ofstream audioFile;
-    audioFile.open(fileName, ios::binary);
-    float amplitude = 0.5;
-    SineOscillator sineOscillator(freq, amplitude);
-
-    // HEADER CHUNK
-    audioFile << "RIFF";
-    audioFile << "----";
-    audioFile << "WAVE";
-
-    // FORMAT CHUNK
-    audioFile << "fmt ";
-    writeToFile(audioFile, 16, 4);                        // Size
-    writeToFile(audioFile, 1, 2);                         // Compression code
-    writeToFile(audioFile, 1, 2);                         // Number of channels
-    writeToFile(audioFile, sampleRate, 4);                // Sample rate
-    writeToFile(audioFile, sampleRate * bitDepth / 8, 4); // Byte rate
-    writeToFile(audioFile, bitDepth / 8, 2);              // Block align
-    writeToFile(audioFile, bitDepth, 2);                  // Bit depth
-
-    // DATA CHUNK
-    audioFile << "data";
-    audioFile << "----";
-
-    int preAudioPosition = audioFile.tellp();
-    auto maxAmplitude = pow(2, bitDepth - 1) - 1;
-    for (int i = 0; i < sampleRate * duration; i++)
+    try
     {
-        auto sample = sineOscillator.process();
-        int intSample = static_cast<int>(sample * maxAmplitude);
-        writeToFile(audioFile, intSample, 3);
+        audioFile.open(fileName, ios::binary);
+
+        float amplitude = 0.5;
+        SineOscillator sineOscillator(freq, amplitude);
+
+        // HEADER CHUNK
+        audioFile << "RIFF";
+        audioFile << "----";
+        audioFile << "WAVE";
+
+        // FORMAT CHUNK
+        audioFile << "fmt ";
+        writeToFile(audioFile, 16, 4);                        // Size
+        writeToFile(audioFile, 1, 2);                         // Compression code
+        writeToFile(audioFile, 1, 2);                         // Number of channels
+        writeToFile(audioFile, sampleRate, 4);                // Sample rate
+        writeToFile(audioFile, sampleRate * bitDepth / 8, 4); // Byte rate
+        writeToFile(audioFile, bitDepth / 8, 2);              // Block align
+        writeToFile(audioFile, bitDepth, 2);                  // Bit depth
+
+        // DATA CHUNK
+        audioFile << "data";
+        audioFile << "----";
+
+        int preAudioPosition = audioFile.tellp();
+        auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+        auto minAmplitude = -maxAmplitude - 1;
+        for (int i = 0; i < sampleRate * duration; i++)
+        {
+            auto sample = sineOscillator.process();
+
+            int intSample = static_cast<int>((sample - (-amplitude)) / (amplitude - (-amplitude)) * (maxAmplitude - minAmplitude) + minAmplitude);
+
+            writeToFile(audioFile, intSample, bitDepth / 8);
+        }
+        int postAudioPosition = audioFile.tellp();
+
+        audioFile.seekp(preAudioPosition - 4);
+        writeToFile(audioFile, postAudioPosition - preAudioPosition, 4);
+
+        audioFile.seekp(4, ios::beg);
+        writeToFile(audioFile, postAudioPosition - 8, 4);
+
+        audioFile.close();
     }
-    int postAudioPosition = audioFile.tellp();
-
-    audioFile.seekp(preAudioPosition - 4);
-    writeToFile(audioFile, postAudioPosition - preAudioPosition, 4);
-
-    audioFile.seekp(4, ios::beg);
-    writeToFile(audioFile, postAudioPosition - 8, 4);
-
-    audioFile.close();
+    catch (ofstream::failure e)
+    {
+        cerr << "Exception opening/writing/closing file.\n";
+        return -1;
+    }
 
     return 0;
 }
@@ -104,7 +117,7 @@ int main()
     cout << directions << endl;
     cout << "Frequency (0 Hz - 24 kHz): " << endl;
     cin >> freq;
-    cout << "Seconds (1 - 30): " << endl;
+    cout << "Seconds (0 - 30): " << endl;
     cin >> duration;
 
     try
